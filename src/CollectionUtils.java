cbf14c5755ecd741a001b4188196f83670dce6ec
PruebasPA <jandpos@cqteduca.edu.com>
2023-02-19 09:46:55 -0500

    medium

--- src/CollectionUtils.java
+++ src/CollectionUtils.java
@@ -1,17 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class CollectionUtils {
-    public static <T> List<T> filldata(Supplier<T> list, int size){
+    public static <T> List<T> filldata(Supplier<T> supplier, Integer size) {
 List<T> list1 = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            list1.add(list.get());
+            list1.add(supplier.get());
         }
         return list1;
-
-
     }
+
+    public static <T,R> List<R> transformdata(Function<T,R> function, List<T> values){
+        List<R> list2 = new ArrayList<>();
+        for(T value: values){
+            list2.add(function.apply(value));
+        }
+        return list2;
+    }
+    public static <T,R> List<R> newlist(Function<T,R> function,List<T> values){
+        return values.stream().map(function).collect(Collectors.toList());
+    }
+    public static <T> List<T> filterlist(List<T> values, Predicate<T> predicate){
+        return values.stream().filter(predicate).collect(Collectors.toList());
+    }
+    public static <T> void impr(List<T> values, Consumer<T> consumer){
+        values.forEach(consumer);
+    }
+    public static <T> T nonlist(List<T> values, BinaryOperator<T> binaryOperator){
+        return values.stream().reduce(binaryOperator).get();
+    }
+
+
 
 
 }
---
+++ src/Interfaces/String_Int.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public class String_Int {
+    public static String strings(){
+        return null;
+    }
+}
